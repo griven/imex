6860729fc88db795d89b6550785f5d0c1c71e779
griven <griveh@yandex.ru>
2015-06-29 16:47:50 +0300

    build transport  file added

---
+++ _build/build.transport.php
@@ -0,0 +1,76 @@
+<?php
+$tstart = explode(' ', microtime());
+$tstart = $tstart[1] + $tstart[0];
+set_time_limit(0);
+
+/* define package names */
+define('PKG_NAME','ImEx');
+define('PKG_NAME_LOWER','imex');
+define('PKG_VERSION','0.1.0');
+define('PKG_RELEASE','pl');
+
+/* define build paths */
+$root = dirname(dirname(__FILE__)).'/';
+$sources = array(
+    'root' => $root,
+    'build' => $root . '_build/',
+    'lexicon' => $root . 'core/components/'.PKG_NAME_LOWER.'/lexicon/',
+    'docs' => $root.'core/components/'.PKG_NAME_LOWER.'/docs/',
+    'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
+    'source_core' => $root.'core/components/'.PKG_NAME_LOWER,
+);
+unset($root);
+
+/* override with your own defines here (see build.config.sample.php) */
+define('MODX_CORE_PATH', dirname(dirname(__FILE__)).'/core/');
+require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+
+$modx= new modX();
+$modx->initialize('mgr');
+echo '<pre>'; /* used for nice formatting of log messages */
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget('ECHO');
+
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+$builder = new modPackageBuilder($modx);
+$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
+$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
+
+/* create category */
+$category= $modx->newObject('modCategory');
+$category->set('id',1);
+$category->set('category',PKG_NAME);
+
+/* create category vehicle */
+$attr = array();
+
+$vehicle = $builder->createVehicle($category,$attr);
+
+$modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to category...');
+$vehicle->resolve('file',array(
+    'source' => $sources['source_assets'],
+    'target' => "return MODX_ASSETS_PATH . 'components/';",
+));
+$vehicle->resolve('file',array(
+    'source' => $sources['source_core'],
+    'target' => "return MODX_CORE_PATH . 'components/';",
+));
+$builder->putVehicle($vehicle);
+
+/* now pack in the license file, readme and setup options */
+$builder->setPackageAttributes(array(
+    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+    'license' => file_get_contents($sources['docs'] . 'license.txt'),
+    'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
+));
+$modx->log(modX::LOG_LEVEL_INFO,'Added package attributes and setup options.');
+
+/* zip up package */
+$modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
+$builder->pack();
+
+$tend= explode(" ", microtime());
+$tend= $tend[1] + $tend[0];
+$totalTime= sprintf("%2.4f s",($tend - $tstart));
+$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
+exit ();
